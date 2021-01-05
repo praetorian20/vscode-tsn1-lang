eb8c97d3b29619a3227d1fe76fe56ec886ce05db
Ashish Sadanandan <ashish.sadanandan@gmail.com>
2021-01-05 00:28:36 -0700

    Add parsing of package directives

--- src/extension.ts
+++ src/extension.ts
@@ -15,14 +15,27 @@ class TsnDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
          for (var i = 0; i < document.lineCount; i++) {
             var line = document.lineAt(i);
+
+            const pkg_re = /\bpackage\b\s+([A-Za-z_][A-Za-z_0-9]*(\.[A-Za-z_][A-Za-z_0-9]*)*);/;
+            var match = line.text.match(pkg_re);
+            if (match) {
+               symbols.push({
+                  name: match[1],
+                  kind: vscode.SymbolKind.Package,
+                  location: new vscode.Location(document.uri, line.range)
+               });
+               continue;
+            }
+
             const enum_re = /\b([A-Za-z_][A-Za-z_0-9]*)\b\s*::=\s*enumerated/;
-            const match = line.text.match(enum_re);
+            match = line.text.match(enum_re);
             if (match) {
                symbols.push({
                   name: match[1],
                   kind: vscode.SymbolKind.Enum,
                   location: new vscode.Location(document.uri, line.range)
-               })
+               });
+               continue;
             }
          }
 
